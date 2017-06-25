038b9ab6769ee546f42efb8cfa86e90b1b9b95c5
satyam kumar <satyamvats5@gmail.com>
2017-06-25 09:57:44 +0530

    chalk: Disabled raw mode on exit
    To disable raw mode on program termination by setting
    back the original terminal attributes
    
    fixes: #1
    
    Signed-off-by: Satyam kumar <satyamvats5@gmail.com"

--- chalk.c
+++ chalk.c
@@ -1,21 +1,26 @@
 #include <unistd.h>
-#include <termios.h>
+#include <stdlib.h>  
+#include <termios.h> 
+
+struct termios orig_termios;
+
+void disableRawMode()
+{
+	tcsetattr(STDIN_FILENO,TCSAFLUSH,&orig_termios);
+}
 
 void enableRawMode()
 {
-	struct termios raw;
-
-	tcgetattr(STDIN_FILENO, &raw);
-
+	tcgetattr(STDIN_FILENO, &orig_termios);
+	atexit(disableRawMode);
+	struct termios raw=orig_termios; 
 	raw.c_lflag &= ~(ECHO);
-
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main()
 {
-	enableRawMode();
-
+	enableRawMode();   
 	char c;
 	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
 	return 0;
