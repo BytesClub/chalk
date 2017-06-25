aa95bc1fe3a9fd0f41155872024938be3a8a4736
satyam <satyamvats5@gmail.com>
2017-06-25 09:35:54 +0530

    disableRawMode
    To disable raw mode on program termination.
    
    fixes: #1
    
    Signed-off-by: Satyam kumar <satyamvats5@gmail.com"

--- chalk.c
+++ chalk.c
@@ -6,23 +6,21 @@ struct termios orig_termios;
 
 void disableRawMode()
 {
-
 	tcsetattr(STDIN_FILENO,TCSAFLUSH,&orig_termios);
 }
+
 void enableRawMode()
 {
 	tcgetattr(STDIN_FILENO, &orig_termios);
 	atexit(disableRawMode);
 	struct termios raw=orig_termios; 
-
 	raw.c_lflag &= ~(ECHO);
-
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main()
 {
-	enableRawMode();
+	enableRawMode();   
 	char c;
 	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
 	return 0;
