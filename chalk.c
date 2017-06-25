173c03bef4ca8d00fd7b2c23d6bc45207cea4eec
satyam <satyamvats5@gmail.com>
2017-06-26 00:29:57 +0530

    updates

--- chalk.c
+++ chalk.c
@@ -16,7 +16,7 @@ void enableRawMode()
 	tcgetattr(STDIN_FILENO, &orig_termios);
 	atexit(disableRawMode);
 	struct termios raw=orig_termios; 
-	raw.c_lflag &= ~(ECHO | ICANON); 
+	raw.c_lflag &= ~(ECHO | ICANON ); 
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
