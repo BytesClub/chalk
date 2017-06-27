f43d8302dad80cfc449711b15804e99cd922dd7a
Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>
2017-06-28 01:42:41 +0530

    read: Display control and non-control characters
    If the character that is read() is a control character, it is directly
    displayed in %d format. On the other hand, if it is a non control
    character, it is printed in both integer and character format.
    
    In this way, we get to see how various keypresses are translated as
    bytes when they are read. Escape sequence is inputs 3 or 4 byte to the
    terminal and starts with 27, '[' followed by one or two characters.
    
    Signed-off-by: Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>

--- chalk.c
+++ chalk.c
@@ -25,8 +25,10 @@ int main()
 	enableRawMode();   
 	char c;
 	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
-		if(!(iscntrl(c))) {
-			printf("%c",c);
+		if(iscntrl(c)) {
+			printf("%d\n",c);
+		} else {
+			printf("%d ('%c')\n", c, c);
 		}
 	}
 	return 0;
