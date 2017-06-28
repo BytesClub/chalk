942570111f90951ea6e8c9e06efc7ba114dea3b9
Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>
2017-06-28 19:15:57 +0530

    flags: Turned off SIGINT and SIGTSTP flags
    The SIGINT and SIGTSTP flags were causing the program to terminate,
    which is why those flags are disabled. Now, Ctrl-C will be read as 3
    byte and Ctrl-z will be read as a 26 byte
    
    This also disables Ctrl+Y for MacOS
    
    Signed-off-by: Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>

--- chalk.c
+++ chalk.c
@@ -15,8 +15,10 @@ void enableRawMode()
 {
 	tcgetattr(STDIN_FILENO, &orig_termios);
 	atexit(disableRawMode);
+
 	struct termios raw=orig_termios; 
-	raw.c_lflag &= ~(ECHO | ICANON);
+	raw.c_lflag &= ~(ECHO | ICANON | ISIG);
+	
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
