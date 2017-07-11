970fb0907810ad77ac887730065a9e87c2b970a1
Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>
2017-07-11 13:30:40 +0530

    indentation: Converted tab = 8 spaces
    To setup the vimrc according to our requirements add these lines to your
    ~/.vimrc file:
    
    filetype plugin indent on
    set tabstop=8
    set shiftwidth=8
    set expandtab
    
    Signed-off-by: Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>

--- chalk.c
+++ chalk.c
@@ -8,41 +8,41 @@ struct termios orig_termios;
 
 void disableRawMode()
 {
-	tcsetattr(STDIN_FILENO,TCSAFLUSH,&orig_termios);
+        tcsetattr(STDIN_FILENO,TCSAFLUSH,&orig_termios);
 }
 
 void enableRawMode()
 {
-	tcgetattr(STDIN_FILENO, &orig_termios);
-	atexit(disableRawMode);
+        tcgetattr(STDIN_FILENO, &orig_termios);
+        atexit(disableRawMode);
 
-	struct termios raw=orig_termios; 
-	raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
-	raw.c_oflag &= ~(OPOST);
-	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-	raw.c_cflag |= (CS8);
-	raw.c_cc[VMIN] = 0; /* Return each byte, or 0 on timeout */
-	raw.c_cc[VTIME] = 1; /* 100 ms timeout */
+        struct termios raw=orig_termios; 
+        raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+        raw.c_oflag &= ~(OPOST);
+        raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+        raw.c_cflag |= (CS8);
+        raw.c_cc[VMIN] = 0; /* Return each byte, or 0 on timeout */
+        raw.c_cc[VTIME] = 1; /* 100 ms timeout */
 
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+        tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main()
 {
-	enableRawMode();
+        enableRawMode();
 
-	while(1) {
-		char c = '\0';
-		read(STDIN_FILENO, &c, 1);
-		if(iscntrl(c)) {
-			printf("%d\r\n",c);
-		} else {
-			printf("%d ('%c')\r\n", c, c);
-		}
+        while(1) {
+                char c = '\0';
+                read(STDIN_FILENO, &c, 1);
+                if(iscntrl(c)) {
+                        printf("%d\r\n",c);
+                } else {
+                        printf("%d ('%c')\r\n", c, c);
+                }
 
-		if (c == 'q')
-			break;
-	}
+                if (c == 'q')
+                        break;
+        }
 
-	return 0;
+        return 0;
 }
