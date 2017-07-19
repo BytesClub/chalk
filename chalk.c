25e039dadf1261abb4d52bb7e63b614d6036761f
Satyam Kumar <satyamvats5@gmail.com>
2017-07-19 13:28:47 +0530

    input: Press Ctrl-Q to quit
    Previously, pressing 'q' used to quit the application, which is now
    changed to Ctrl-Q
    
    The CTRL_KEY macro performs a logincal AND with the value 00011111,
    which mirrors the function the Ctrl key performs in the terminal, ie to
    strip the bits 5 and 6 from whatever the key that is pressed in the
    terminal
    
    Signed-off-by: SATYAM KUMAR <satyamvats5@gmail.com>
    Signed-off-by: Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>

--- chalk.c
+++ chalk.c
@@ -7,6 +7,10 @@
 #include <ctype.h>
 #include <errno.h>
 
+/* defines */
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 /* data */
 
 struct termios orig_termios;
@@ -64,7 +68,7 @@ int main()
                         printf("%d ('%c')\r\n", c, c);
                 }
 
-                if (c == 'q')
+                if (c == CTRL_KEY('q'))
                         break;
         }
 
