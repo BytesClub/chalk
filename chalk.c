2dcf85f121a49b7f018e7f87a888f7591c2aad17
SATYAM KUMAR <satyamvats5@gmail.com>
2017-07-21 03:16:56 +0530

    output: clear the screen on exit
    Whenever the program terminates we clear the
    screen and repostion the cursor using escape
    sequence "\x1b[2J" and "\x1b[H" respectively.
    
    fixes: https://github.com/BytesClub/chalk/issues/19
    
    Signed-off-by: SATYAM KUMAR <satyamvats5@gmail.com>
    Signed-off-by: Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>

--- chalk.c
+++ chalk.c
@@ -20,6 +20,8 @@ struct termios orig_termios;
 void die(const char *S ) 
 {
         perror(S);
+        write(STDOUT_FILENO, "\x1b[2J", 4);
+        write(STDOUT_FILENO,"\x1b[H", 3);
         exit(1);
 }
 
@@ -77,6 +79,8 @@ void editorProcessKeypress()
 
         switch(c) {
                 case CTRL_KEY('q'): 
+                        write(STDOUT_FILENO, "\x1b[2J", 4);
+                        write(STDOUT_FILENO, "\x1b[H", 3);
                         exit(0);
                         break;
         }
