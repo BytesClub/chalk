14a2bdd8a4bf94c2f105c31ac2a7652b3564e0a2
Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>
2017-07-20 19:33:05 +0530

    input: read and process input from the user
    editorReadKey() waits for input from the user and then returns it, while
    editorProcessKeypress() gets the keypress and handles it.
    
    editorProcessKeypress() is called infinitely while the editor is running
    
    fixes: https://github.com/BytesClub/chalk/issues/16
    
    Signed-off-by: Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>

--- chalk.c
+++ chalk.c
@@ -50,6 +50,30 @@ void enableRawMode()
         }
 }
 
+char editorReadKey()
+{
+        int nread;
+        char c;
+        while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+                if (nread == -1 && errno != EAGAIN)
+                        die("read");
+        }
+        return c;
+}
+
+/* input */
+
+void editorProcessKeypress()
+{
+        char c = editorReadKey();
+
+        switch(c) {
+                case CTRL_KEY('q'): 
+                        exit(0);
+                        break;
+        }
+}
+
 /* init */
 
 int main()
@@ -57,19 +81,7 @@ int main()
         enableRawMode();
 
         while(1) {
-                char c = '\0';
-                if(read(STDIN_FILENO, &c, 1) == -1 &&  errno != EAGAIN ) {
-                        die("read");
-                }
-
-                if(iscntrl(c)) {
-                        printf("%d\r\n",c);
-                } else {
-                        printf("%d ('%c')\r\n", c, c);
-                }
-
-                if (c == CTRL_KEY('q'))
-                        break;
+                editorProcessKeypress();
         }
 
         return 0;
